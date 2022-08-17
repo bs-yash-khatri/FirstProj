f39adfe7fe32f33e3585b671d1269901d2cf7c0c
“Khatri <yash.khatri@bombaysoftwares.com>
2022-08-17 15:19:33 +0530

    commit - 2

--- app/src/main/java/gthb/libgt/githblibry/MainActivity.kt
+++ app/src/main/java/gthb/libgt/githblibry/MainActivity.kt
@@ -2,10 +2,14 @@ package gthb.libgt.githblibry
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import gthb.libgt.githublib.MathProb
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        println(MathProb.calculateSum(20, 21).toString())
+
     }
 }
---
+++ githublib/src/main/java/gthb/libgt/githublib/MathProb.kt
@@ -0,0 +1,11 @@
+package gthb.libgt.githublib
+
+//fun main(){
+//    print(MathProb.calculateSum(11, 10).toString())
+//}
+
+object MathProb {
+    fun calculateSum(a: Int, b: Int): Int {
+        return a + b
+    }
+}
